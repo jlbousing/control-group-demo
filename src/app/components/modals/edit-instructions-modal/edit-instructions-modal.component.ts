0ecae7a6003b5597fbab2685eb3a954125644f7a
jorge <jbousing@gmail.com>
2022-07-28 01:56:21 -0400

    maquetacion inicial de modal para editar instrucciones

--- src/app/components/modals/edit-instructions-modal/edit-instructions-modal.component.ts
+++ src/app/components/modals/edit-instructions-modal/edit-instructions-modal.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-edit-instructions-modal',
+  selector: 'edit-instructions-modal',
   templateUrl: './edit-instructions-modal.component.html',
   styleUrls: ['./edit-instructions-modal.component.scss']
 })
 export class EditInstructionsModalComponent implements OnInit {
 
+  @Input('showModal') showModal: boolean = false;
+  @Output() emiterModal = new EventEmitter<boolean>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onCloseModal(){
+    this.emiterModal.emit(true);
   }
 
 }
