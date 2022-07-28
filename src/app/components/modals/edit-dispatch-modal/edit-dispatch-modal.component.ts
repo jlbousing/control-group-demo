6e7947445b5fdd9608cde9e361799f896f61c061
jorge <jbousing@gmail.com>
2022-07-28 10:14:55 -0400

    maquetacion inicial de modal para editar despachos

--- src/app/components/modals/edit-dispatch-modal/edit-dispatch-modal.component.ts
+++ src/app/components/modals/edit-dispatch-modal/edit-dispatch-modal.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-edit-dispatch-modal',
+  selector: 'edit-dispatch-modal',
   templateUrl: './edit-dispatch-modal.component.html',
   styleUrls: ['./edit-dispatch-modal.component.scss']
 })
 export class EditDispatchModalComponent implements OnInit {
 
+  @Input('showModal') showModal: boolean = false;
+  @Output() emiterModal = new EventEmitter<boolean>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onCloseModal(){
+    this.emiterModal.emit(true);
   }
 
 }
