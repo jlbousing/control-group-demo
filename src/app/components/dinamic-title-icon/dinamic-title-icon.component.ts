23609590983d11ce84167c0528dc913995736528
jorge <jbousing@gmail.com>
2022-08-20 17:34:42 -0400

    iconos dinamicos

---
+++ src/app/components/dinamic-title-icon/dinamic-title-icon.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { IIconTitleList } from 'src/app/interfaces/IIConTitleList';
+import { iconTitleList } from 'src/app/utils/IconTitleList';
+
+@Component({
+  selector: 'dinamic-title-icon',
+  templateUrl: './dinamic-title-icon.component.html',
+  styleUrls: ['./dinamic-title-icon.component.scss']
+})
+export class DinamicTitleIconComponent implements OnInit, OnChanges {
+
+  @Input("title") title: string = "";
+  currentTitle: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    this.currentTitle = this.getCurrentIcon(this.title)
+  }
+
+  getCurrentIcon(title: string) {
+
+    var className: any = "";
+
+    className = iconTitleList.find((item: IIconTitleList) => {
+      return item.title === title;
+    })?.iconClass;
+
+    return className;
+  }
+
+}
