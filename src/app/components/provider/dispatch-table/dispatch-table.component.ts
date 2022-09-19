68036abf8f9b82a45fdf7b9f3bcee592afba255f
jorge <jbousing@gmail.com>
2022-09-19 05:24:27 -0400

    se dinamiza tabla de despachos

--- src/app/components/provider/dispatch-table/dispatch-table.component.ts
+++ src/app/components/provider/dispatch-table/dispatch-table.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IDispatch } from 'src/app/interfaces/IDispacht';
+import { IProduction } from 'src/app/interfaces/IProduction';
 
 @Component({
   selector: 'dispatch-table',
@@ -7,7 +9,9 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class DispatchTableComponent implements OnInit {
 
-  @Output() emiterCategoriesTable = new EventEmitter<any>();
+  @Input("dispatchs") dispatchs: IDispatch[] = [];
+  @Input("production") production: IProduction | null = null;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -15,7 +19,6 @@ export class DispatchTableComponent implements OnInit {
 
   selectEdit(){
     let category = {};
-    this.emiterCategoriesTable.emit(category);
   }
 
 }
--- src/app/pages/providers/dispatch/dispatch.component.ts
+++ src/app/pages/providers/dispatch/dispatch.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Dialog } from '@angular/cdk/dialog';
-import { CreateDispatchModalComponent } from 'src/app/components/modals/create-dispatch-modal/create-dispatch-modal.component';
-import { EditDispatchModalComponent } from 'src/app/components/modals/edit-dispatch-modal/edit-dispatch-modal.component';
+import { IAssignament } from 'src/app/interfaces/IAssignament';
+import { IRecipe } from 'src/app/interfaces/IRecipe';
+import { IProduction } from 'src/app/interfaces/IProduction';
+import { DispachtService } from 'src/app/services/dispatch/dispacht.service';
+import { AssignamentService } from 'src/app/services/assignaments/assignament.service';
+import { RecipesService } from 'src/app/services/recipes/recipes.service';
+import { ProductionService } from 'src/app/services/production/production.service';
+import { ActivatedRoute } from '@angular/router';
+import { IDispatch } from 'src/app/interfaces/IDispacht';
 
 @Component({
   selector: 'app-dispatch',
@@ -10,31 +17,66 @@ import { EditDispatchModalComponent } from 'src/app/components/modals/edit-dispa
 })
 export class DispatchComponent implements OnInit {
 
-  showModalCreate: boolean = false;
-  showModalEdit: boolean = false;
+  supplierId: number = 0;
+  assignaments: IAssignament[] = [];
+  recipes: IRecipe[] = [];
+  productions: IProduction[] = [];
+  dispatchs: IDispatch[] = [];
 
-  constructor(public dialog: Dialog) { }
+  assignament: IAssignament | null = null;
+  recipe: IRecipe | null = null;
+  production: IProduction | null = null;
+
+
+  constructor(
+    public dialog: Dialog,
+    private route: ActivatedRoute,
+    private dispatchService: DispachtService,
+    private assignamentService: AssignamentService,
+    private recipeService: RecipesService,
+    private productionService: ProductionService
+  ) { }
 
 
   ngOnInit(): void {
+
+    this.route.params.subscribe(params => {
+      this.supplierId = params["supplierId"];
+
+      this.assignamentService.getAssignamentsBySupplier(this.supplierId)
+        .subscribe((response: IAssignament[]) => {
+          this.assignaments = response;
+        })
+    });
   }
 
-  showCreateModal() {
-    this.dialog.open(CreateDispatchModalComponent);
+  setAssignament(value: any) {
+    let assignament = <IAssignament> value;
+    this.assignament = assignament;
+    console.log("probando asignacion ",value)
+
+    this.recipeService.getRecipes(50,0,assignament.id).subscribe((response: IRecipe[]) => {
+      this.recipes = response;
+      console.log("mostrando recipes ",this.recipes)
+    })
   }
 
-  closeModalCreate(msg: boolean){
-    console.log("hey uya");
-    this.showModalCreate = false;
+  setRecipe(value: any) {
+    this.recipe = <IRecipe> value;
+    this.productionService.getProductionByRecipe(50,0,this.recipe.id)
+      .subscribe((response: IProduction[]) => {
+        this.productions = response;
+      })
   }
 
-  showEditModal(obj: any) {
-    this.dialog.open(EditDispatchModalComponent);
-  }
+  setProduction(value: any) {
+    this.production = <IProduction> value;
 
-  closeModalEdit(msg: boolean){
-    console.log("hey uya");
-    this.showModalEdit = false;
+    this.dispatchService.getDispatchsByProductionId(50,0,this.production.id)
+      .subscribe((response: IDispatch[]) => {
+        this.dispatchs = response;
+        console.log("probando despachos ",this.dispatchs);
+      });
   }
 
 }
