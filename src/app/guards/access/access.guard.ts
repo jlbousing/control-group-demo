1e5049d5587cd281dace48e95b03434684755afe
jorge <jbousing@gmail.com>
2022-10-02 13:48:38 -0400

    mejorando guard

--- src/app/guards/access/access.guard.ts
+++ src/app/guards/access/access.guard.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Route, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AccessService } from 'src/app/services/access/access.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccessGuard implements CanActivate {
 
-  constructor(private accessService: AccessService) {}
+  constructor(
+    private accessService: AccessService,
+    private router: Router
+  ) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.accessService.canCreate();
+
+    if(this.accessService.canCreate()){
+      return true;
+    }else {
+      this.router.navigateByUrl("/login");
+    }
+
+    return false;
   }
 
 }
